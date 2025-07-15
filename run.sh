e233439cb39c57f3ab05343d87fb2b3ece1c0a35
Jeonghoon Song <120718733+hon121215@users.noreply.github.com>
2025-07-15 19:18:07 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 # SMIYC AnomalyTrack 
 CUDA_VISIBLE_DEVICES=1 python3 anomaly_utils/anomaly_inference.py \
-    --input '/root/Mask2Anomaly/datasets/RoadAnomaly21/images/*.png' \
-    --config-file '/root/Mask2Anomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' \
-    --output '/root/Mask2Anomaly/results/RoadAnomaly21_anomaly_250701_anomaly_score_map'
+    --input '/root/Objectomaly/datasets/RoadAnomaly21/images/*.png' \
+    --config-file '/root/Objectomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' \
+    --output '/root/Objectomaly/results/RoadAnomaly21_anomaly'
 
 # SMIYC RoadObsticle
 CUDA_VISIBLE_DEVICES=1 python3 anomaly_utils/anomaly_inference.py \
-    --input '/root/Mask2Anomaly/datasets/RoadObsticle21/images/*.webp' \
-    --config-file '/root/Mask2Anomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' \
-    --output '/root/Mask2Anomaly/results/RoadObsticle21_anomaly_250701_anomaly_score_map'
+    --input '/root/Objectomaly/datasets/RoadObsticle21/images/*.webp' \
+    --config-file '/root/Objectomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' \
+    --output '/root/Objectomaly/results/RoadObsticle21_anomaly'
 
 # fs_static
 # CUDA_VISIBLE_DEVICES=1 python3 anomaly_utils/anomaly_inference.py \
-#     --input '/root/Mask2Anomaly/datasets/fs_static/images/*.jpg' \
-#     --config-file '/root/Mask2Anomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' \
-#     --output '/root/Mask2Anomaly/results/fs_static_anomaly_250620_inference' 
+#     --input '/root/Objectomaly/datasets/fs_static/images/*.jpg' \
+#     --config-file '/root/Objectomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' \
+#     --output '/root/Objectomaly/results/fs_static_anomaly' 
 
 # RoadAnomaly
 # CUDA_VISIBLE_DEVICES=1 python3 anomaly_utils/anomaly_inference.py \
-#     --input '/root/Mask2Anomaly/datasets/RoadAnomaly/images/*.jpg' \
-#     --config-file '/root/Mask2Anomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' \
-#     --output '/root/Mask2Anomaly/results/RoadAnomaly_anomaly_250620_inference'
+#     --input '/root/Objectomaly/datasets/RoadAnomaly/images/*.jpg' \
+#     --config-file '/root/Objectomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' \
+#     --output '/root/Objectomaly/results/RoadAnomaly_anomaly'
